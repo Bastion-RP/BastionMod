1cd851f8fdd75c4c4692bcbbf9e68c0c72806bb8
Yohan Riou <yohan.riou@gmail.com>
2020-03-26 21:47:54 +0000

    Adding Guns + Uniforms fixes

---
+++ Guns/ATTACHMENTS/MicroT1/config.cpp
@@ -0,0 +1,87 @@
+﻿////////////////////////////////////////////////////////////////////
+//DeRap: P:\BastionMod\Guns\ATTACHMENTS\MicroT1\config.bin
+//Produced from mikero's Dos Tools Dll version 7.60
+//https://mikero.bytex.digital/Downloads
+//'now' is Thu Mar 26 21:17:42 2020 : 'file' last modified on Thu Mar 26 21:17:42 2020
+////////////////////////////////////////////////////////////////////
+
+#define _ARMA_
+
+class CfgPatches
+{
+	class BastionRP_t1
+	{
+		units[] = {"BastionRP_microt1"};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"DZ_Data","DZ_Weapons_Optics"};
+	};
+};
+class cfgVehicles
+{
+	class Inventory_Base;
+	class ItemOptics: Inventory_Base{};
+	class BastionRP_microt1: ItemOptics
+	{
+		scope = 2;
+		displayName = "Micro T1 Aimpoint";
+		descriptionShort = "";
+		model = "BastionMod\Guns\ATTACHMENTS\MicroT1\microt1.p3d";
+		attachments[] = {"BatteryD"};
+		animClass = "Binoculars";
+		simulation = "itemoptics";
+		inventorySlot = "WeaponOptics";
+		selectionFireAnim = "zasleh";
+		memoryPointCamera = "eyeScope";
+		cameraDir = "cameraDir";
+		rotationFlags = 17;
+		reversed = 0;
+		weight = 250;
+		itemSize[] = {2,1};
+		dispersionModifier = -0.009;
+		dispersionCondition = "true";
+		recoilModifier[] = {1,1,1};
+		hiddenSelections[] = {"reddot"};
+		hiddenSelectionsTextures[] = {""};
+		class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints = 100;
+					healthLevels[] = {{1,{"DZ\weapons\attachments\optics\data\red_dot_ca.paa","DZ\weapons\attachments\optics\data\Red_Dot.rvmat"}},{0.7,{}},{0.5,{"DZ\weapons\attachments\optics\data\lensglass_damage_ca.paa","DZ\weapons\attachments\optics\data\Red_Dot_damage.rvmat"}},{0.3,{}},{0,{"DZ\weapons\attachments\optics\data\lensglass_destruct_ca.paa","DZ\weapons\attachments\optics\data\Red_Dot_destruct.rvmat"}}};
+				};
+			};
+		};
+		class OpticsInfo
+		{
+			memoryPointCamera = "eyeScope";
+			cameraDir = "cameraDir";
+			modelOptics = "-";
+			opticsDisablePeripherialVision = 0.67;
+			opticsFlare = 1;
+			opticsPPEffects[] = {};
+			opticSightTexture = "dz\weapons\attachments\optics\data\collimdot_red_ca.paa";
+			opticsZoomMin = 0.5236;
+			opticsZoomMax = 0.5236;
+			opticsZoomInit = 0.5236;
+			distanceZoomMin = 200;
+			distanceZoomMax = 200;
+			discreteDistance[] = {200};
+			discreteDistanceInitIndex = 0;
+			PPMaskProperties[] = {0.5,0.5,0.4,0.05};
+			PPLensProperties[] = {1,0.15,0,0};
+			PPBlurProperties = 0.2;
+		};
+		repairableWithKits[] = {5,7};
+		repairCosts[] = {30,25};
+		class EnergyManager
+		{
+			hasIcon = 1;
+			energyUsagePerSecond = 0.02;
+			plugType = 1;
+			attachmentAction = 1;
+		};
+	};
+};
