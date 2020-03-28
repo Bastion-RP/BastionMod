6725e244436e837c2133b5e8d60153523f801eff
Kerkkoh <superihippo@gmail.com>
2020-03-28 14:15:26 +0200

    CCTV Monitors spawning, CCTV Camera positions, fixed menu buttons

--- BastionCCTV/scripts/4_World/CCTV/CCTVMonitor.c
+++ BastionCCTV/scripts/4_World/CCTV/CCTVMonitor.c
@@ -1,21 +1,15 @@
-class CCTVCamera {
+class CCTVMonitor {
     protected vector position;
     protected float yaw, pitch, roll;
-    protected bool canRotate;
 
-    void CCTVCamera( vector _position, float _startingAngle = 0.0, float _pitch = 0.0, float _roll = 0.0, bool _canRotate = true ) {
+    void CCTVMonitor( vector _position, float _startingAngle = 0.0, float _pitch = 0.0, float _roll = 0.0 ) {
         position = _position;
         yaw = Math.NormalizeAngle( _startingAngle );
         pitch = Math.NormalizeAngle( _pitch );
         roll = Math.NormalizeAngle( _roll );
-        canRotate = _canRotate;
     }
 
     vector GetPosition() { return position; }
-    float GetStartingAngle() { return yaw; }
-    float GetPitch() { return pitch; }
-    float GetRoll() { return roll; }
-    bool GetCanRotate() { return canRotate; }
     vector GetDirection() {
         vector dir = vector.Zero;
         dir[0] = yaw;
