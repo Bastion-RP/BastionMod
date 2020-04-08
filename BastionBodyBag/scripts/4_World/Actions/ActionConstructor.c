b80e124b95dc26e85fbaa21f0ccb6773cd5bb94b
Kerkkoh <superihippo@gmail.com>
2020-04-08 11:42:35 +0300

    Moved bodybag models into BastionMod pbo and fixed bodybag creation
    Also now removes the bodybag kit when used and you can bury them in a way that removes them completely.

--- BastionBodyBag/scripts/4_World/Actions/ActionConstructor.c
+++ BastionBodyBag/scripts/4_World/Actions/ActionConstructor.c
@@ -2,5 +2,6 @@ modded class ActionConstructor {
     override void RegisterActions(TTypenameArray actions) { 
         super.RegisterActions(actions);
         actions.Insert(ActionBodyBag);
+        actions.Insert(ActionBuryBodyBag);
     }
 }
---
+++ BastionBodyBag/scripts/4_World/ItemBase/Shovel.c
@@ -0,0 +1,9 @@
+modded class Shovel
+{
+	override void SetActions()
+	{
+		super.SetActions();
+
+		AddAction(ActionBuryBodyBag);
+	}
+}
