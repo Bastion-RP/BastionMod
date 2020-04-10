e40d70ecbbac2b42911e93511db2b103559805e8
OGRoomService <OGRoomService@users.noreply.github.com>
2020-04-10 15:35:13 -0400

    Removed whitelist stuff, not necessary naymore.

--- Whitelist/scripts/3_Game/Constants/stringconst.c
+++
@@ -1,8 +0,0 @@
-class BSTWConst {
-    static const string bstProfileDir = "$profile:\\Bastion";
-    static const string bstWhitelistDir = "$profile:\\Bastion\\Whitelist.json";
-    static const string bstNCCSpawnDir = "$profile:\\Bastion\\NCCSpawnPoints.json";
-    static const string bstFileType = ".json";
-    static const string debugPrefix = "[WhiteList Debug] ";
-}
-//static const string m_DebugPrefix = "[Whitelist Debug] ";
