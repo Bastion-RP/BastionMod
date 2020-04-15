0f233b03f06ef8236adb7b3a8a8816c380d53c63
Helkhiana <helkhiana@gmail.com>
2020-04-16 00:58:00 +0100

    isf platecarrier and phone

--- BastionMod/NCCUniform/config.cpp
+++ BastionMod/NCCUniform/config.cpp
@@ -15,3 +15,32 @@ class CfgPatches
 		};
 	};
 };
+
+
+class CfgMods
+{
+	class NCC_Clothing
+	{
+		dir = "NCC_Clothing";
+		picture = "";
+		action = "";
+		hideName = 1;
+		hidePicture = 1;
+		name = "NCC_Clothing";
+		credits = "";
+		author = "Helkhiana";
+		authorID = "0";
+		version = "1.0";
+		extra = 0;
+		type = "mod";
+		dependencies[] = {"World"};
+		class defs
+		{
+			class worldScriptModule
+			{
+				value = "";
+				files[] = {"BastionMod/NCCUniform/scripts/4_World"};
+			};
+		};
+	};
+};
--- BastionMod/Phone/config.cpp
+++ BastionMod/Phone/config.cpp
@@ -1,12 +1,3 @@
-////////////////////////////////////////////////////////////////////
-//DeRap: BastionRPitems\config.bin
-//Produced from mikero's Dos Tools Dll version 7.26
-//https://bytex.market/products/item/weodpphdknnzm70o0h8q/Mikero%27s%20Dos%20Tools
-//'now' is Fri Jul 19 01:04:59 2019 : 'file' last modified on Sun Jul 14 06:31:11 2019
-////////////////////////////////////////////////////////////////////
-
-#define _ARMA_
-
 class CfgPatches
 {
 	class BastionRP_Phone
@@ -17,6 +8,35 @@ class CfgPatches
 		requiredAddons[] = {"DZ_Data"};
 	};
 };
+
+class CfgMods
+{
+	class BastionRP_Phone
+	{
+		dir = "BastionMod";
+		picture = "";
+		action = "";
+		hideName = 1;
+		hidePicture = 1;
+		name = "BastionRP_Phone";
+		credits = "Helkhiana";
+		author = "Kerkkoh";
+		authorID = "0";
+		version = "1.0";
+		extra = 0;
+		type = "mod";
+		dependencies[] = {"World"};
+		class defs
+		{
+			class worldScriptModule
+			{
+				value = "";
+				files[] = {"BastionMod/Phone/scripts/4_World"};
+			};
+		};
+	};
+};
+
 class CfgVehicles
 {
 	class Inventory_Base;
@@ -25,7 +45,41 @@ class CfgVehicles
 		scope = 2;
 		displayName = "Phone";
 		descriptionShort = "Phone";
-		model = "\BastionMod\Phone\Bastion_Phone.p3d";
+		model = "\BastionMod\Phone\data\Bastion_Phone.p3d";
+		weight=250;
+		itemSize[]={2,2};
+		itemBehaviour=1;
+		rotationFlags=1;
+		physLayer="item_large";
+		inventorySlot[] = {"WalkieTalkie", "BRP_Phone"};
+		hiddenSelections[]=
+		{
+			"all"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"BastionMod\Phone\data\bastionphone_co.paa"
+		};
+	};
+};
+class CfgSlots
+{
+	class Slot_BRP_Phone
+	{
+		name = "BRP_Phone";
+		displayName = "BRP_Phone";
+		ghostIcon = "walkietalkie";
+	};
+};
+
+class CfgNonAIVehicles
+{
+	class ProxyAttachment;
+	class ProxyBastion_Phone: ProxyAttachment
+	{
+		scope = 2;
+		inventorySlot[] = {"BRP_Phone"};
+		model = "\BastionMod\Phone\data\Bastion_Phone.p3d";
 	};
 };
 
