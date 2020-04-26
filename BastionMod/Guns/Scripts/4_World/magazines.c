03e80f9724ac12ede08c8d558e0afe68f0d768ba
Jordan447 <jordan447@hotmail.co.uk>
2020-04-26 19:47:17 +0100

    Removed broken scripts, added new scripts
    Removed broken scripts, added new scripts

--- BastionMod/Guns/Scripts/4_World/magazines.c
+++ BastionMod/Guns/Scripts/4_World/magazines.c
@@ -2,3 +2,4 @@ class Mag_ASVAL_20Rnd: MagazineStorage {};
 class Mag_TEC9_20Rnd: MagazineStorage {};
 class Mag_CSP9_17Rnd: MagazineStorage {};
 class Magazine_CSC45_30Rnd: MagazineStorage {};
+class BRP_M1911_Magazine: MagazineStorage {};
