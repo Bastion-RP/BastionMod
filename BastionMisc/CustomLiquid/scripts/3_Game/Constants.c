cd828e203fc6df283732cbf3f8f22299bbb77f20
Helkhiana <helkhiana@gmail.com>
2020-05-04 22:05:26 +0100

    AION custom liquid

---
+++ BastionMisc/CustomLiquid/scripts/3_Game/Constants.c
@@ -0,0 +1,1 @@
+const int LIQUID_AION = 131072;
--- BastionVendor_Data/AION/config.cpp
+++ BastionVendor_Data/AION/config.cpp
@@ -35,20 +35,58 @@ class CfgMods
 		};
 	};
 };
+
+class cfgLiquidDefinitions
+{
+	class AION
+	{
+		type=131072;
+		displayName="AION";
+		class Nutrition
+		{
+			fullnessIndex=1.75;
+			energy=1500;
+			water=500;
+			nutritionalIndex=1;
+			toxicity=0;
+			digestibility=2;
+		};
+	};
+};
 class CfgVehicles
 {
-	class WaterBottle;
-	class BRP_AION: WaterBottle
+	class Bottle_Base;
+	class BRP_AION: Bottle_Base
 	{
-		scope = 2;
-		displayName = "AION";
-		descriptionShort = "Aion | All-In-One NutritionAion. Developed by the NATO-CSTO Coalition.  Is an affordable, sustainable, all-in-one solution to the food crisis that we face every day. In order to regrow a population in a world wracked with disaster, it is imperative that our citizens have access to nutrition that is easy to consume and easy to sustain, so no one in any Bastion goes hungry.Each serving contains 25% daily value of protein and fats, slow-burning carbohydrates, and 28 essential vitamins and minerals that are then mixed with purified water and bottled to ensure that every citizen remains happy and, most importantly, healthy.";
-		model = "BastionMod\BastionVendor_Data\AION\aion.p3d";
-		weight = 25;
-		itemSize[] = {1,3};
-		varQuantityInit = 500;
-		varQuantityMin = 0;
-		varQuantityMax = 500;
+		scope=2;
+		displayName="AION";
+		descriptionShort="Aion | All-In-One NutritionAion. Developed by the NATO-CSTO Coalition.  Is an affordable, sustainable, all-in-one solution to the food crisis that we face every day. In order to regrow a population in a world wracked with disaster, it is imperative that our citizens have access to nutrition that is easy to consume and easy to sustain, so no one in any Bastion goes hungry.Each serving contains 25% daily value of protein and fats, slow-burning carbohydrates, and 28 essential vitamins and minerals that are then mixed with purified water and bottled to ensure that every citizen remains happy and, most importantly, healthy.";
+		model="BastionMod\BastionVendor_Data\AION\aion.p3d";
+		weight=25;
+		itemSize[]={1,3};
+		stackedRandom=0;
+		stackedUnit="";
+		varLiquidTypeInit=131072;
+		varQuantityInit=500;
+		varQuantityMin=0;
+		varQuantityMax=500;
+		liquidContainerType="131072";
+		isMeleeWeapon=1;
+		hiddenSelections[] = 
+		{
+			"cap",
+			"body"
+		};
+		hiddenSelectionsTextures[] = 
+		{
+			"BastionMod\BastionVendor_Data\aion\data\cap_co.paa",			
+			"BastionMod\BastionVendor_Data\aion\data\label_co.paa"
+		};
+		hiddenSelectionsMaterials[] = 
+		{
+			"BastionMod\BastionVendor_Data\aion\data\aion.rvmat",			
+			"BastionMod\BastionVendor_Data\aion\data\aion.rvmat"
+		};
 		class DamageSystem
 		{
 			class GlobalHealth
@@ -56,17 +94,40 @@ class CfgVehicles
 				class Health
 				{
 					hitpoints = 100;
-					healthLevels[] = {{1,{"DZ\gear\drinks\data\Loot_WaterBottle.rvmat"}},{0.7,{"DZ\gear\drinks\data\Loot_WaterBottle.rvmat"}},{0.5,{"DZ\gear\drinks\data\Loot_WaterBottle_damage.rvmat"}},{0.3,{"DZ\gear\drinks\data\Loot_WaterBottle_damage.rvmat"}},{0,{"DZ\gear\drinks\data\Loot_WaterBottle_destruct.rvmat"}}};
+					healthLevels[] = 
+					{
+						{
+							1.0,
+							{"BastionMod\BastionVendor_Data\AION\data\aion.rvmat","BastionMod\BastionVendor_Data\AION\data\aion.rvmat"}
+						},
+						{
+							0.7,
+							{"BastionMod\BastionVendor_Data\AION\data\aion.rvmat","BastionMod\BastionVendor_Data\AION\data\aion.rvmat"}
+						},
+						{
+							0.5,
+							{"BastionMod\BastionVendor_Data\AION\data\aion_damage.rvmat","BastionMod\BastionVendor_Data\AION\data\aion_damage.rvmat"}
+						},
+						{
+							0.3,
+							{"BastionMod\BastionVendor_Data\AION\data\aion_damage.rvmat","BastionMod\BastionVendor_Data\AION\data\aion_damage.rvmat"}
+						},
+						{
+							0.0,
+							{"BastionMod\BastionVendor_Data\AION\data\aion_destruct.rvmat","BastionMod\BastionVendor_Data\AION\data\aion_destruct.rvmat"}
+						}
+					};
 				};
 			};
 		};
 		class Nutrition
 		{
-			fullnessIndex = 1.5;
-			energy = 100;
-			water = 0;
-			nutritionalIndex = 1;
-			toxicity = 0;
+			totalVolume=1;
+			fullnessIndex=1.75;
+			energy=1500;
+			water=500;
+			nutritionalIndex=1;
+			toxicity=0;
 		};
 		class AnimEvents
 		{
