f99d3f65ad130adedb4597ef758cee926abc6144
Jordan447 <jordan447@hotmail.co.uk>
2020-05-26 17:50:53 +0100

    fixes
    fixes

--- BastionGuns/config.cpp
+++ BastionGuns/config.cpp
@@ -7,8 +7,7 @@ class CfgPatches
 		requiredVersion=0.1;
 		requiredAddons[]=
 		{
-			"DZ_Data",
-			"DZ_Weapons_Projectiles"
+			"DZ_Data"
 		};
 	};
 };
@@ -37,37 +36,6 @@ class CfgMods
 			{
 				value = "";
 				files[] = {"BastionMod/BastionGuns/scripts/4_World"};
-			};
-		};
-	};
-};
-
-
-class CfgAmmo
-{
-	class Bullet_12GaugeSlug;
-	class Bullet_12GaugeRubberSlug: Bullet_12GaugeSlug
-	{
-		class DamageApplied
-		{
-			type="Projectile";
-			dispersion=0.050000001;
-			bleedThreshold=0;
-			class Health
-			{
-				damage=0;
-			};
-			class Blood
-			{
-				damage=0;
-			};
-			class Shock
-			{
-				damage=80;
-				damageOverride[]=
-				{
-					{0.60000002,1}
-				};
 			};
 		};
 	};
