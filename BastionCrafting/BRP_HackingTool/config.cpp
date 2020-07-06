9b0f56912403f2566ad2c7675db6ecb98952b23c
Lugge2 <52799807+lugge22@users.noreply.github.com>
2020-07-06 03:23:10 +0200

    Added a Hacking tool

---
+++ BastionCrafting/BRP_HackingTool/config.cpp
@@ -0,0 +1,99 @@
+class CfgPatches
+{
+	class BRP_Hackingtool
+	{
+		units[]=
+		{
+			"BRP_Hackingtool"
+		};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"DZ_Data"
+		};
+	};
+	
+};
+
+class CfgVehicles
+{		
+	class Inventory_Base;
+	
+	class Book_Base;
+	class ItemBook: Book_Base{};
+	class BRP_Hackingtool: ItemBook
+	{
+		scope=2;
+		model="\BastionMod\BastionCrafting\BRP_HackingTool\BRP_HackingTool.p3d";
+		displayName="Crafted Hacking Tool";
+		descriptionShort="A Crafted Hacking Tool that meight open some door";
+		itemSize[]={2,2};
+		weight = 500;
+		rotationFlags=1;
+		absorbency=0.89999998;
+		physLayer="item_small";
+		hiddenSelections[]=
+		{
+			"all"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			""
+		};
+		class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints=100;
+					healthLevels[]=
+					{
+						
+						{
+							1,
+							
+							{
+								"BastionMod\BastionCrafting\BRP_Magazine\data\brp_magazine.rvmat"
+							}
+						},
+						
+						{
+							0.69999999,
+							
+							{
+								"BastionMod\BastionCrafting\BRP_Magazine\data\brp_magazine.rvmat"
+							}
+						},
+						
+						{
+							0.5,
+							
+							{
+								"BastionMod\BastionCrafting\BRP_Magazine\data\brp_magazine_damage.rvmat"
+							}
+						},
+						
+						{
+							0.30000001,
+							
+							{
+								"BastionMod\BastionCrafting\BRP_Magazine\data\brp_magazine_damage.rvmat"
+							}
+						},
+						
+						{
+							0,
+							
+							{
+								"BastionMod\BastionCrafting\BRP_Magazine\data\brp_magazine_destruct.rvmat"
+							}
+						}
+					};
+				};
+			};
+		};
+		
+	};
+};
