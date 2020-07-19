89e613c47ad17c9b795e33d7c16361029aea7d10
Inkota <57881942+Inkota@users.noreply.github.com>
2020-07-19 19:07:12 +0300

    Anim for CSSMG-9 mag

--- BastionGuns/scripts/4_World/Animations.c
+++ BastionGuns/scripts/4_World/Animations.c
@@ -7,5 +7,13 @@ modded class JMAnimRegister
 		pType.AddItemInHandsProfileIK("BRP_R700_Base", "dz/anims/workspaces/player/player_main/weapons/player_main_winchester70.asi", pBehavior, "dz/anims/anm/player/ik/weapons/winchester70.anm", "dz/anims/anm/player/reloads/Winchester70/w_winchester70_states.anm");
  		pType.AddItemInHandsProfileIK("BRP_CSSMG9_Base", "dz/anims/workspaces/player/player_main/weapons/player_main_m4a1.asi", pBehavior, "dz/anims/anm/player/ik/weapons/m4a1_ik.anm", "dz/anims/anm/player/reloads/M4A1/w_M4A1_states.anm");
 
-   }
+	}
+   
+     override void RegisterOneHanded(DayZPlayerType pType, DayzPlayerItemBehaviorCfg pBehavior)
+    {
+    	super.RegisterOneHanded(pType, pBehavior);
+    	pBehavior.SetToolsOneHanded();
+		
+		pType.AddItemInHandsProfileIK("BRP_Mag_CSSMG9_30Rnd", "dz/anims/workspaces/player/player_main/props/player_main_mp5k_mag.asi", pBehavior, "dz/anims/anm/player/ik/attachments/magazines/magazine_mp5_30rnd.anm");
+	}
 };
