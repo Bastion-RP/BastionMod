3db6112f250e8c2df6eb286b6b10a9d6994100d7
KyHbKa <sashakulichev47@gmail.com>
2020-07-20 19:07:28 +1000

    assign smg9 isf

--- BastionMisc/ISFWeaponLock/scripts/4_world/ISFWeapons.c
+++ BastionMisc/ISFWeaponLock/scripts/4_world/ISFWeapons.c
@@ -16,6 +16,12 @@ class Csc45_Base : AKS74U {
     }
 }
 
+modded class BRP_CSSMG9_Base {
+    override bool IsISFWeapon() {
+        return true;
+    }
+}
+
 class BRP_CSR556 extends CSR556_Base {}
 class BRP_CSP9 extends CSP9_Base {}
 class BRP_CSC45 extends Csc45_Base {}
