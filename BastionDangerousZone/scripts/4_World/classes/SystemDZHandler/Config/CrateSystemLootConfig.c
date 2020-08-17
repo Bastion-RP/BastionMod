0ade78a2462e9f11347358801dc82daadd036d2c
KyHbKa <sashakulichev47@gmail.com>
2020-08-17 14:46:27 +1000

    fix crates

--- BastionDangerousZone/scripts/4_World/classes/SystemDZHandler/Config/CrateSystemLootConfig.c
+++ BastionDangerousZone/scripts/4_World/classes/SystemDZHandler/Config/CrateSystemLootConfig.c
@@ -2,21 +2,19 @@ class DZLootConfig
 {
 	string LootName;
 	float  ProbToSpawn;
-	ref array<ref DZAttachmentConfig> AttachmentsToLoot;
-	//AttachmentsToItem = NULL;
+	ref array<ref DZAttachmentConfig> AttachmentsToLoot = new array< ref DZAttachmentConfig>();
 
+	// void DZLootConfig(string lootname, float probtospawn)
+	// {
+	// 	LootName = lootname;
+	// 	ProbToSpawn = probtospawn;
+	// 	AttachmentsToLoot = new ref array< ref DZAttachmentConfig>;
+	// }
 
-	void DZLootConfig(string lootname, float probtospawn)
-	{
-		LootName = lootname;
-		ProbToSpawn = probtospawn;
-		AttachmentsToLoot = new ref array< ref DZAttachmentConfig>;
-	}
-
-	void AddAttachmentsToItem(string name, float prob)
-	{
-		AttachmentsToLoot.Insert(new ref DZAttachmentConfig(name,prob));
-	}
+	// void AddAttachmentsToItem(string name, float prob)
+	// {
+	// 	AttachmentsToLoot.Insert(new ref DZAttachmentConfig(name,prob));
+	// }
 }
 
 class DZAttachmentConfig
@@ -24,25 +22,25 @@ class DZAttachmentConfig
 	string AttachName;
 	float  ProbAttachToSpawn;
 
-	void  DZAttachmentConfig(string attachname, float probattachtospawn)
-	{
-		AttachName = attachname;
-		ProbAttachToSpawn = probattachtospawn;
-	}
+	// void  DZAttachmentConfig(string attachname, float probattachtospawn)
+	// {
+	// 	AttachName = attachname;
+	// 	ProbAttachToSpawn = probattachtospawn;
+	// }
 }
 
 class DZListLootConfig{
 	string Name;
-	ref array<ref DZLootConfig> Loots;
+	ref array<ref DZLootConfig> Loots = new array<ref DZLootConfig>();
 
-	void DZListLootConfig(string name)
-	{
-		Loots= new ref array<ref DZLootConfig>;
-		Name=name;
-	}
+	// void DZListLootConfig(string name)
+	// {
+	// 	Loots= new ref array<ref DZLootConfig>;
+	// 	Name=name;
+	// }
 
-	void AddLootConfig(string lootname, float probtospawn)
-	{
-		Loots.Insert(new ref DZLootConfig(lootname,probtospawn));
-	}
+	// void AddLootConfig(string lootname, float probtospawn)
+	// {
+	// 	Loots.Insert(new ref DZLootConfig(lootname,probtospawn));
+	// }
 }
