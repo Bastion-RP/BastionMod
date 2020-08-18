4b4d29e8f2862e8d942daca5e26d316f99ad9326
Bryan Rode <51579862+OGRoomService@users.noreply.github.com>
2020-08-17 22:03:00 -0400

    ISF gear configurable
    Very basic. It will spawn an item and not a count

--- BastionMultiCharacters/scripts/3_Game/Constants/ContString.c
+++ BastionMultiCharacters/scripts/3_Game/Constants/ContString.c
@@ -1,7 +1,9 @@
 class MCConst {
-    static const string loadoutDir = "$profile:\\Bastion\\Loadouts";
-    static const string spawnPointDir = "$profile:\\Bastion\\SpawnPoints.json";
-    static const string isfSpawnPointDir = "$profile:\\Bastion\\ISFSpawnPoints.json";
+    static const string modDir = "$profile:\\Bastion\\MultiCharacters";
+    static const string loadoutDir = modDir + "\\Characters";
+    static const string spawnPointDir = modDir + "\\SpawnPoints.json";
+    static const string isfSpawnGearDir = modDir + "\\ISFSpawnGear.json";
+    static const string isfSpawnPointDir = modDir + "\\ISFSpawnPoints.json";
     static const string debugPrefix = "[MULTICHAR DEBUG] ";
     static const string fileType = ".json";
 }
