f1a0e2915d35a617ab10968e8fd3e7b30d07459e
KyHbKa <sashakulichev47@gmail.com>
2020-08-21 23:19:56 +1000

    quest teleport

--- BastionQuestsSystem/scripts/4_World/QuestHud/QuestElementsLayout/ChoiceContainer.c
+++ BastionQuestsSystem/scripts/4_World/QuestHud/QuestElementsLayout/ChoiceContainer.c
@@ -6,19 +6,22 @@ class ChoiceContainer
 	private EditBoxWidget			m_ApplyIDStg;
 	private EditBoxWidget			m_ChoiceText;
 	private EditBoxWidget			m_FinishText;
+	private EditBoxWidget			m_TeleportTo;
 
-	void ChoiceContainer( Widget parent, string text, int move = 0, int apl = 0, int fin = 0)
+	void ChoiceContainer( Widget parent, string text, int move = 0, int apl = 0, int fin = 0, string teleportTo = "")
 	{
 		m_Root			= GetGame().GetWorkspace().CreateWidgets("BastionMod/BastionQuestsSystem/layouts/ChoiceWidget.layout", parent);
 		m_MoveToStg		= EditBoxWidget.Cast(m_Root.FindAnyWidget("MoveToStg"));
 		m_ApplyIDStg	= EditBoxWidget.Cast(m_Root.FindAnyWidget("ApplyIDStg"));
 		m_ChoiceText	= EditBoxWidget.Cast(m_Root.FindAnyWidget("ChoiceText"));
 		m_FinishText	= EditBoxWidget.Cast(m_Root.FindAnyWidget("FinishStg"));
+		m_TeleportTo	= EditBoxWidget.Cast(m_Root.FindAnyWidget("TeleportTo"));
 		m_Root.SetText(text);
 		m_ChoiceText.SetText(text);
 		m_MoveToStg.SetText(move.ToString());
 		m_ApplyIDStg.SetText(apl.ToString());
 		m_FinishText.SetText(fin.ToString());
+		m_TeleportTo.SetText(teleportTo);
 	}
 
 }
