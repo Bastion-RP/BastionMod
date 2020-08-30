25a887a59f3664d52d4c2bcfa0e2c4460ec63220
Inkota <57881942+Inkota@users.noreply.github.com>
2020-08-30 07:00:36 +0300

    Changed mag capacity to 20 rnd

--- BastionGuns/SCAR_H/config.cpp
+++ BastionGuns/SCAR_H/config.cpp
@@ -177,12 +177,12 @@ class cfgMagazines
 	class BRP_Mag_SCARH_30rnd: Magazine_Base
 	{
 		scope = 2;
-		displayName = "30rd SCAR-H Mag";
-		descriptionShort = "Detachable box magazine for the FN SCAR-H. Holds up to 30 rounds of 7.62x51mm.";
+		displayName = "20rd SCAR-H Mag";
+		descriptionShort = "Detachable box magazine for the FN SCAR-H. Holds up to 20 rounds of 7.62x51mm.";
 		model = "BastionMod\BastionGuns\SCAR_H\scar_h_mag.p3d";
 		weight = 400;
 		itemSize[] = {1,3};
-		count = 30;
+		count = 20;
 		ammo = "Bullet_308Win";
 		ammoItems[] = {"Ammo_308Win","Ammo_308WinTracer"};
 		tracersEvery = 0;
