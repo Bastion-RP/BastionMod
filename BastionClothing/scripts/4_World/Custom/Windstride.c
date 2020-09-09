f7126600c1df1f10df51a20f3d37ea460f02a16d
Jadfii <Jadfii@users.noreply.github.com>
2020-09-09 23:21:52 +0100

    Add wetsuit/invisible pants functionality

--- BastionClothing/scripts/4_World/Custom/Windstride.c
+++ BastionClothing/scripts/4_World/Custom/Windstride.c
@@ -1,4 +1,48 @@
-class Wetsuit_Base: Clothing {};
+class Wetsuit_Base: Clothing
+{
+	PlayerBase m_Player;
+	Invisible_Pants m_InvisPants;
+
+	override bool CanPutAsAttachment(EntityAI parent)
+	{
+		return super.CanPutAsAttachment(parent) && !parent.FindAttachmentBySlotName("Legs");
+	}
+
+	override void OnWasAttached(EntityAI parent, int slot_id)
+	{
+		super.OnWasAttached(parent, slot_id);
+
+		Class.CastTo(m_Player, parent.GetHierarchyRootPlayer());
+		if (m_Player.GetInventory()) Class.CastTo(m_InvisPants, m_Player.GetInventory().CreateInInventory("Invisible_Pants"));
+	}
+	
+	override void OnWasDetached(EntityAI parent, int slot_id)
+	{
+		super.OnWasDetached(parent, slot_id);
+
+		if (m_InvisPants)
+		{
+			m_InvisPants.Delete();
+			m_InvisPants = null;
+		}
+	}
+};
+
+class Invisible_Pants: Clothing
+{
+	override bool CanDetachAttachment(EntityAI parent)
+	{
+		return false;
+	}
+
+	override void OnWasDetached(EntityAI parent, int slot_id)
+	{
+		super.OnWasDetached(parent, slot_id);
+
+		Delete();
+	}
+};
+
 class BRP_Tactical_Jacket_Base: Clothing {};
 class BRP_Tactical_Jacket_Tan: BRP_Tactical_Jacket_Base {};
 class BRP_Tactical_Jacket_Black: BRP_Tactical_Jacket_Base {};
