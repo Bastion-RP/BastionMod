0328b83aeed83f07672019b0e2d2c6e08d0a6061
Jadfii <Jadfii@users.noreply.github.com>
2020-09-19 22:48:57 +0100

    Fix player name on main menu and cleanup

--- BastionMenu/scripts/5_Mission/gui/newui/mainmenu/Moddedmainmenu.c
+++ BastionMenu/scripts/5_Mission/gui/newui/mainmenu/Moddedmainmenu.c
@@ -1,46 +1,7 @@
 modded class MainMenu extends UIScriptedMenu
 {
-	protected ref MainMenuStats		m_Stats;
-	protected ref MainMenuVideo		m_Video;
-	
-	protected MissionMainMenu		m_Mission;
-	protected DayZIntroScenePC 		m_ScenePC;
-	
-	protected TextWidget			m_PlayerName;
-	protected TextWidget			m_Version;
-	
-	protected Widget				m_CharacterRotationFrame;
-	
-	protected Widget				m_Play;
-	protected Widget		 		m_ChooseServer;
-	protected Widget				m_CustomizeCharacter;
-	protected Widget				m_PlayVideo;
-	protected Widget				m_Tutorials;
-	protected Widget				m_TutorialButton;
-	protected Widget				m_MessageButton;
-	protected Widget				m_SettingsButton;
-	protected Widget				m_Exit;
-	protected Widget				m_NewsMain;
-	protected Widget				m_NewsSec1;
-	protected Widget				m_NewsSec2;
-	protected Widget				m_PrevCharacter;
-	protected Widget				m_NextCharacter;
-	
-	protected Widget				m_LastPlayedTooltip;
-	protected Widget				m_LastPlayedTooltipLabel;
-	protected TextWidget 			m_LastPlayedTooltipName;
-	protected TextWidget			m_LastPlayedTooltipIP;
-	protected TextWidget			m_LastPlayedTooltipPort;
-
-	protected Widget 				m_Forums;
-	
-	protected ref WidgetFadeTimer	m_LastPlayedTooltipTimer;
-	protected ref Widget			m_LastFocusedButton;
-	
-	protected ref TextWidget		m_ModdedWarning;
-	protected ref ModsMenuSimple	m_ModsSimple;
-	protected ref ModsMenuDetailed	m_ModsDetailed;
-	protected ref ModsMenuTooltip	m_ModsTooltip;
+	protected Widget m_Forums;
+	protected TextWidget m_WelcomeBack;
 
 	override Widget Init()
 	{
@@ -88,7 +49,9 @@ modded class MainMenu extends UIScriptedMenu
 		
 		m_PlayVideo.Show( false );
 		
-		m_PlayerName				= TextWidget.Cast( layoutRoot.FindAnyWidget("character_name_text") );
+		m_WelcomeBack = TextWidget.Cast(layoutRoot.FindAnyWidget("WelcomeBack"));
+		m_PlayerName = TextWidget.Cast(layoutRoot.FindAnyWidget("character_name_text"));
+		SetPlayerName();
 		
 		// Set Version
 		string version;
@@ -109,209 +72,37 @@ modded class MainMenu extends UIScriptedMenu
 		
 		return layoutRoot;
 	}
-	
-	void ~MainMenu()
-	{
-		
-	}
-	
-	void LoadMods()
-	{
-		ref array<ref ModInfo> modArray = new array<ref ModInfo>;		
-		GetGame().GetModInfos( modArray );
 
-		if( modArray.Count() > 0 )
+	void SetPlayerName()
+	{
+		if (m_PlayerName && m_WelcomeBack)
 		{
-			modArray.Remove( modArray.Count() - 1 );
-			modArray.Invert();
-		}
-		
-		if( m_ModsSimple )
-			delete m_ModsSimple;
-		if( m_ModsDetailed )
-			delete m_ModsDetailed;
-		
-		m_ModdedWarning.Show( GetGame().GetModToBeReported() );
-		
-		if( modArray.Count() > 0 )
-		{
-			layoutRoot.FindAnyWidget("ModsSimple").Show( true );
-			m_ModsTooltip = new ModsMenuTooltip(layoutRoot);
+			string name;
+			GetCLIParam("name", name);
+			if (!name) name = "Survivor";
 
-			m_ModsDetailed = new ModsMenuDetailed(modArray, layoutRoot.FindAnyWidget("ModsDetailed"), m_ModsTooltip, this);
-			
-			m_ModsSimple = new ModsMenuSimple(modArray, layoutRoot.FindAnyWidget("ModsSimple"), m_ModsDetailed);
+			m_PlayerName.SetText(name);
 		}
 	}
-	
-	override bool OnMouseButtonDown( Widget w, int x, int y, int button )
+
+	override void OnChangeCharacter()
 	{
-		if ( w == m_CharacterRotationFrame )
+		super.OnChangeCharacter();
+		SetPlayerName();
+	}
+	
+	override bool OnClick(Widget w, int x, int y, int button)
+	{
+		if (button == MouseState.LEFT && w == m_Forums)
 		{
-			if (m_ScenePC)
-				m_ScenePC.CharacterRotationStart();
+			GetGame().OpenURL("https://bastionrp.com/forums/");
 			return true;
 		}
-		else if ( w == m_Play )
-		{
-			m_LastFocusedButton = m_Play;
-			Play();
-			return true;
-		}
-		else if ( w == m_ChooseServer )
-		{
-			m_LastFocusedButton = m_ChooseServer;
-			OpenMenuServerBrowser();
-			return true;
-		}
-		
-		return false;
+
+		return super.OnClick(w, x, y, button);
 	}
 	
-	override bool OnMouseButtonUp( Widget w, int x, int y, int button )
-	{
-		if (m_ScenePC)
-			m_ScenePC.CharacterRotationStop();
-		return false;
-	}
-	
-	override bool OnClick( Widget w, int x, int y, int button )
-	{
-		if( button == MouseState.LEFT )
-		{
-			if( w == m_Play )
-			{
-				m_LastFocusedButton = m_Play;
-				g_Game.ConnectFromServerBrowser( "51.81.48.230", 2302, "lou2goty" ); // Stable Server
-				return true;
-			}
-			else if ( w == m_ChooseServer )
-			{
-				m_LastFocusedButton = m_ChooseServer;
-				OpenMenuServerBrowser();
-				return true;
-			}
-			else if ( w == m_CustomizeCharacter )
-			{
-				OpenMenuCustomizeCharacter();
-				return true;
-			}
-			else if ( w == m_TutorialButton )
-			{
-				OpenTutorials();
-				return true;
-			}
-			else if ( w == m_MessageButton )
-			{
-				OpenCredits();
-				return true;
-			}
-			else if ( w == m_SettingsButton )
-			{
-				OpenSettings();
-				return true;
-			}
-			else if ( w == m_Exit )
-			{
-				Exit();
-				return true;
-			}
-			else if ( w == m_PrevCharacter )
-			{
-				PreviousCharacter();
-				return true;
-			}
-			else if ( w == m_NextCharacter )
-			{
-				NextCharacter();
-				return true;
-			}
-			else if ( w == m_PlayVideo )
-			{
-				m_LastFocusedButton = m_PlayVideo;
-				PlayVideo();
-				return true;
-			}
-			else if ( w == m_Tutorials )
-			{
-				m_LastFocusedButton = m_Tutorials;
-				OpenTutorials();
-				return true;
-			}
-			else if ( w == m_Forums )
-			{
-				GetGame().OpenURL("https://bastionrp.com/forums/");
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	override bool OnMouseEnter( Widget w, int x, int y )
-	{
-		if( w == m_Play )
-		{
-			string ip = "";
-			string name = "";
-			int port = 0;
-			 
-			if(m_ScenePC && !m_ScenePC.GetIntroCharacter().IsDefaultCharacter())
-			{
-				int charID = m_ScenePC.GetIntroCharacter().GetCharacterID();
-				m_ScenePC.GetIntroCharacter().GetLastPlayedServer(charID, ip, name, port);
-				
-				m_LastPlayedTooltipName.SetText( "#server_details_name " + name );
-				m_LastPlayedTooltipIP.SetText( "#main_menu_IP " + ip );
-				m_LastPlayedTooltipPort.SetText( "#main_menu_port " + port );
-				
-				m_LastPlayedTooltipTimer.FadeIn( m_LastPlayedTooltip, 0.3, true );
-			}
-		}
-		
-		if( IsFocusable( w ) )
-		{
-			ColorHighlight( w );
-			return true;
-		}
-		return false;
-	}
-	
-	override bool OnMouseLeave( Widget w, Widget enterW, int x, int y )
-	{
-		if( w == m_Play )
-		{
-			m_LastPlayedTooltipTimer.FadeOut( m_LastPlayedTooltip, 0.3, true );
-		}
-		
-		if( IsFocusable( w ) )
-		{
-			ColorNormal( w );
-			return true;
-		}
-		return false;
-	}
-	
-	override bool OnFocus( Widget w, int x, int y )
-	{
-		if( IsFocusable( w ) )
-		{
-			ColorHighlight( w );
-			return true;
-		}
-		return false;
-	}
-	
-	override bool OnFocusLost( Widget w, int x, int y )
-	{
-		if( IsFocusable( w ) )
-		{
-			ColorNormal( w );
-			return true;
-		}
-		return false;
-	}
-	
-	bool IsFocusable( Widget w )
+	override bool IsFocusable( Widget w )
 	{
 		if( w )
 		{
@@ -333,272 +124,10 @@ modded class MainMenu extends UIScriptedMenu
 		return false;
 	}
 	
-	override void Refresh()
+	override void Play()
 	{
-		string name;
-		
-		if( m_ScenePC )
-		{
-			OnChangeCharacter();
-			//name = m_ScenePC.GetIntroCharacter().GetCharacterName();
-		}		
-		
-		string version;
-		GetGame().GetVersion( version );
-		m_Version.SetText( "#main_menu_version" + " " + version );
-	}	
-	
-	override void OnShow()
-	{
-		SetFocus( null );
-		OnChangeCharacter();
-		LoadMods();
-		return;
-		/*
-		GetDayZGame().GetBacklit().MainMenu_OnShow();
-	
-		SetFocus( layoutRoot );
-		g_Game.SetLoadState( DayZLoadState.MAIN_MENU_CONTROLLER_SELECT );
-		
-		Refresh();
-		
-		if( m_ScenePC && m_ScenePC.GetIntroCamera() )
-		{
-			m_ScenePC.GetIntroCamera().LookAt( m_ScenePC.GetIntroCharacter().GetPosition() + Vector( 0, 1, 0 ) );
-		}
-		
-		LoadMods();
-		*/
-	}
-	
-	override void OnHide()
-	{
-		GetDayZGame().GetBacklit().MainMenu_OnHide();
-		//super.OnHide();
-	}
-	
-	override void Update(float timeslice)
-	{
-		super.Update( timeslice );
-		
-		if( GetGame() && GetGame().GetInput() && GetGame().GetInput().LocalPress("UAUIBack", false) )
-		{
-			Exit();
-		}
-	}
-	
-	void Play()
-	{
-		if (m_ScenePC && m_ScenePC.GetIntroCharacter())
-		{
-			//saves new, unplayed demounit for further use
-			if (CanSaveCharacterSetup())
-			{
-				m_ScenePC.GetIntroCharacter().SaveCharacterSetup();
-			}
-		}
-	
-		if( !g_Game.IsNewCharacter() )
-		{
-			GetGame().GetCallQueue(CALL_CATEGORY_GUI).CallByName(this, "ConnectLastSession");
-		}
-		else
-		{
-			GetGame().GetCallQueue(CALL_CATEGORY_GUI).CallByName(this, "ConnectBestServer");
-		}
-	}
-	
-	bool CanSaveCharacterSetup()
-	{
-		if (m_ScenePC && m_ScenePC.GetIntroCharacter() && m_ScenePC.GetIntroCharacter().GetCharacterID() == -1)
-		{
-			PlayerBase player = m_ScenePC.GetIntroCharacter().GetCharacterObj();
-			if (player && player.GetInventory() && player.GetInventory().FindAttachment(InventorySlots.BODY)) //default equipment detected
-			{
-				return true;
-			}
-		}
-		return false;
-	}
-
-	void OpenMenuServerBrowser()
-	{
-		EnterScriptedMenu(MENU_SERVER_BROWSER);
-				
-		//saves demounit for further use
-		if (m_ScenePC && m_ScenePC.GetIntroCharacter() && m_ScenePC.GetIntroCharacter().GetCharacterID() == -1 )
-		{
-			PlayerBase player = m_ScenePC.GetIntroCharacter().GetCharacterObj();
-			if(player && player.GetInventory() && player.GetInventory().FindAttachment(InventorySlots.BODY))
-			{
-				m_ScenePC.GetIntroCharacter().SaveCharacterSetup();
-			}
-		}
-	}
-	
-	void OpenMenuCustomizeCharacter()
-	{
-		EnterScriptedMenu(MENU_CHARACTER);
-	}
-	
-	void NextCharacter()
-	{
-		if ( m_ScenePC && m_ScenePC.GetIntroCharacter() )
-		{
-			int charID = m_ScenePC.GetIntroCharacter().GetNextCharacterID();
-			//m_ScenePC.GetIntroCharacter().SaveCharName();
-			if( charID != m_ScenePC.GetIntroCharacter().GetCharacterID())
-			{
-				m_ScenePC.GetIntroCharacter().SetCharacterID(charID);
-				OnChangeCharacter();
-			}
-		}
-	}
-	
-	void PreviousCharacter()
-	{
-		if ( m_ScenePC && m_ScenePC.GetIntroCharacter() )
-		{
-			//m_ScenePC.GetIntroCharacter().SaveCharName();
-			int charID = m_ScenePC.GetIntroCharacter().GetPrevCharacterID();
-			if( charID != m_ScenePC.GetIntroCharacter().GetCharacterID())
-			{
-				m_ScenePC.GetIntroCharacter().SetCharacterID(charID);
-				OnChangeCharacter();
-			}
-		}
-	}
-	
-	void OnChangeCharacter()
-	{
-		if ( m_ScenePC && m_ScenePC.GetIntroCharacter() )
-		{
-			int charID = m_ScenePC.GetIntroCharacter().GetCharacterID();
-			m_ScenePC.GetIntroCharacter().CreateNewCharacterById( charID );
-			m_PlayerName.SetText( m_ScenePC.GetIntroCharacter().GetCharacterNameById( charID ) );
-			
-			Widget w = m_CustomizeCharacter.FindAnyWidget(m_CustomizeCharacter.GetName() + "_label");
-			
-			if ( w )
-			{
-				TextWidget text = TextWidget.Cast( w );
-				
-				if( m_ScenePC.GetIntroCharacter().IsDefaultCharacter() )
-				{
-					text.SetText("#layout_main_menu_customize_char");
-				}
-				else
-				{
-					text.SetText("#layout_main_menu_rename");
-				}
-			}
-			if (m_ScenePC.GetIntroCharacter().GetCharacterObj() )
-			{
-				if ( m_ScenePC.GetIntroCharacter().GetCharacterObj().IsMale() )
-					m_ScenePC.GetIntroCharacter().SetCharacterGender(ECharGender.Male);
-				else
-					m_ScenePC.GetIntroCharacter().SetCharacterGender(ECharGender.Female);
-			}
-			
-			//update character stats
-			m_Stats.UpdateStats();
-		}
-	}
-	
-	void OpenStats()
-	{
-		
-	}
-	
-	void OpenMessages()
-	{
-		
-	}
-	
-	void OpenSettings()
-	{
-		EnterScriptedMenu(MENU_OPTIONS);
-	}
-	
-	void PlayVideo()
-	{
-		EnterScriptedMenu(MENU_VIDEO);
-	}
-	
-	void OpenTutorials()
-	{
-		EnterScriptedMenu(MENU_TUTORIAL);
-	}
-	
-	void OpenCredits()
-	{
-		EnterScriptedMenu(MENU_CREDITS);
-	}
-	
-	void Exit()
-	{
-		GetGame().GetUIManager().ShowDialog("#main_menu_exit", "#main_menu_exit_desc", IDC_MAIN_QUIT, DBT_YESNO, DBB_YES, DMT_QUESTION, this);
-	}
-		
-	bool TryConnectLastSession( out string ip, out int port )
-	{
-		if( g_Game.GetLastVisitedServer( ip, port ) )
-		{
-			return true;
-		}
-		return false;
-	}
-	
-	void AutoConnect()
-	{
-		OnlineServices.AutoConnectToEmptyServer();
-	}
-	
-	void ConnectLastSession()
-	{
-		string ip = "";
-		string name = "";
-		int port = 0;
-			 
-		if(!m_ScenePC.GetIntroCharacter().IsDefaultCharacter())
-		{
-			int charID = m_ScenePC.GetIntroCharacter().GetCharacterID();
-			m_ScenePC.GetIntroCharacter().GetLastPlayedServer(charID,ip,name,port);
-		}
-		
-		if( ip.Length() > 0 )
-		{
-			g_Game.ConnectFromServerBrowser( ip, port, "" );
-		}
-		else
-		{
-			OpenMenuServerBrowser();
-		}
-	}
-	
-	void ConnectBestServer()
-	{
-		ConnectLastSession();
-	}
-	
-	override bool OnModalResult( Widget w, int x, int y, int code, int result )
-	{
-		if( code == IDC_MAIN_QUIT )
-		{
-			if( result == 2 )
-				GetGame().GetCallQueue(CALL_CATEGORY_GUI).Call(g_Game.RequestExit, IDC_MAIN_QUIT);
-			#ifdef PLATFORM_WINDOWS
-			if( result == 3 )
-				ColorNormal( GetFocus() );
-			#endif
-			return true;
-		}
-		else if( code == 555 )
-		{
-			if( result == 2 )
-				OpenTutorials();
-		}
-		return false;
+		//g_Game.ConnectFromServerBrowser("51.81.48.230", 2302, "lou2goty");
+		g_Game.ConnectFromServerBrowser("5.80.209.49", 2302, "lou2goty");
 	}
 		
 	//Coloring functions (Until WidgetStyles are useful)
@@ -623,20 +152,6 @@ modded class MainMenu extends UIScriptedMenu
 		ImagenSetColor(w, color_img);
 	}
 	
-	void ColorNormal( Widget w )
-	{
-		if( !w )
-			return;
-		
-		int color_pnl = ARGB(0, 0, 0, 0);
-		int color_lbl = ARGB(255, 255, 255, 255);
-		int color_img = ARGB(255, 255, 255, 255);
-		
-		ButtonSetColor(w, color_pnl);
-		ButtonSetTextColor(w, color_lbl);
-		ImagenSetColor(w, color_img);
-	}
-	
 	override void ButtonSetText( Widget w, string text )
 	{
 		if( !w )
@@ -649,56 +164,5 @@ modded class MainMenu extends UIScriptedMenu
 			label.SetText( text );
 		}
 		
-	}
-	
-	void ButtonSetColor( Widget w, int color )
-	{
-		if( !w )
-			return;
-		
-		Widget panel = w.FindWidget( w.GetName() + "_panel" );
-		
-		if( panel )
-		{
-			panel.SetColor( color );
-		}
-	}
-	
-	void ImagenSetColor( Widget w, int color )
-	{
-		if( !w )
-			return;
-		
-		Widget panel = w.FindWidget( w.GetName() + "_image" );
-		
-		if( panel )
-		{
-			panel.SetColor( color );
-		}
-	}
-	
-	void ButtonSetTextColor( Widget w, int color )
-	{
-		if( !w )
-			return;
-
-		TextWidget label	= TextWidget.Cast(w.FindAnyWidget( w.GetName() + "_label" ) );
-		TextWidget text		= TextWidget.Cast(w.FindAnyWidget( w.GetName() + "_text" ) );
-		TextWidget text2	= TextWidget.Cast(w.FindAnyWidget( w.GetName() + "_text_1" ) );
-				
-		if( label )
-		{
-			label.SetColor( color );
-		}
-		
-		if( text )
-		{
-			text.SetColor( color );
-		}
-		
-		if( text2 )
-		{
-			text2.SetColor( color );
-		}
 	}
 }
