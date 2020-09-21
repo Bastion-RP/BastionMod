695388a10a4456e26d4377097a4adc64af7ef9ef
Jadfii <Jadfii@users.noreply.github.com>
2020-09-21 23:08:59 +0100

    Remove AION box filling until fix

--- BastionVendor/scripts/4_world/BRP_AIONBox.c
+++ BastionVendor/scripts/4_world/BRP_AIONBox.c
@@ -2,21 +2,6 @@ class BRP_AIONBox_Base extends Inventory_Base{}
 class BRP_AIONBox_Full extends BRP_AIONBox_Base
 {
 	void BRP_AIONBox_Full(){}
-
-	void EEInit()
-	{
-		if (GetInventory()) FillBox();
-	}
-
-	void FillBox()
-	{
-		bool full = HasAnyCargo();
-		while (!full)
-		{
-			EntityAI item;
-			full = !(Class.CastTo(item, GetInventory().CreateEntityInCargo("BRP_AION")));
-		}
-	}
 }
 
 class BRP_AIONBoxes1 extends BuildingBase
