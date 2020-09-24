1935c0d372a95ceed4e28ae1a46898e57a7b0675
KyHbKa <sashakulichev47@gmail.com>
2020-09-24 15:41:22 +1000

    test branch

--- BastionRadiation/scripts/3_Game/Consts.c
+++ BastionRadiation/scripts/3_Game/Consts.c
@@ -9,10 +9,16 @@ class RadConst
 	static const string RADIATION_SUITS_FILE = "Suits.json";
 
 	static const float DECREASE_SUIT_DAMAGE_RATE = 0.05; //95% reducing koef the difference between the critical threshold and external radiation
+
+	static const int SICKNESS_THRESHOLD_T1 = 250;
+	static const int SICKNESS_THRESHOLD_T2 = 600;
+	static const int SICKNESS_THRESHOLD_T3 = 1200;
+	static const int SICKNESS_THRESHOLD_T4 = 2000;
 }
 
 enum RadRPCs
 {
 	REQUEST_DATA = -4000404,
 	RESPONSE_DATA,
+	SEND_RADIATION_DATA,
 }
