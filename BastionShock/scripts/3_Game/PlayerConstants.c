38a104c85b5a9aeea7e740101f603700ddea386f
BastionRP Dev Server <55332345+bastionrp@users.noreply.github.com>
2020-10-04 14:03:57 -0700

    Update PlayerConstants.c

--- BastionShock/scripts/3_Game/PlayerConstants.c
+++ BastionShock/scripts/3_Game/PlayerConstants.c
@@ -1,5 +1,5 @@
 modded class PlayerConstants {
-	static const float SHOCK_REFILL_CONSCIOUS_SPEED			= 1.75;		//shock refill speed when the player is conscious
+	static const float SHOCK_REFILL_CONSCIOUS_SPEED			= 1.5;		//shock refill speed when the player is conscious
 
 	// adjust this to make player wake up slower or faster
 	static const float SHOCK_REFILl_UNCONSCIOUS_SPEED		= 1.0;		//shock refill speed when the player is unconscious
@@ -9,7 +9,7 @@ modded class PlayerConstants {
 	static const float SL_HEALTH_CRITICAL = 30;
 	static const float SL_HEALTH_LOW = 70;
 	static const float SL_HEALTH_NORMAL = 100;
-	static const float SL_HEALTH_HIGH = 150;
+	static const float SL_HEALTH_HIGH = 120;
 
 
 	// health regen, can be increased to make player regen faster
