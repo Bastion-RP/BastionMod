26eedaa4ebc00483759048368efaeecf09d1034d
Dmitry Astakhov <deamouse@outlook.com>
2020-10-15 23:53:56 +0300

    ISF Weapon Lock
    fix

--- BastionMisc/ISFWeaponLock/scripts/4_world/Modded/WeaponManager.c
+++ BastionMisc/ISFWeaponLock/scripts/4_world/Modded/WeaponManager.c
@@ -1,10 +1,11 @@
 modded class WeaponManager
 {
 	ref array<int> ISFClasses = {5, 6, 7, 8, 9, 10, 11};
+	bool		   soundStart;
 
 	override bool CanFire(Weapon_Base wpn)
 	{
-		if (wpn.IsISFWeapon() && !wpn.IsHacked())
+		/*if (wpn.IsISFWeapon() && !wpn.IsHacked())
 		{
 			PlayerBase player = PlayerBase.Cast(m_player);
 			if ( player )
@@ -19,7 +20,42 @@ modded class WeaponManager
 				sound.SetSoundAutodestroy(true);
 			}
 			return false;
-		}
+		}*/
 		return super.CanFire(wpn);
+	}
+	
+	override void Fire(Weapon_Base wpn)
+	{
+		if (wpn.IsISFWeapon() && !wpn.IsHacked())
+		{
+			PlayerBase player = PlayerBase.Cast(m_player);
+			if ( player )
+			{
+				if ( (ISFClasses.Find(player.GetMultiCharactersPlayerClass()) != -1) )
+				{
+					super.Fire(wpn);
+					return;
+				}
+				else
+				{
+					if(!soundStart)
+					{
+						soundStart = true;
+						player.BSTShowNotification("Cannot fire ID locked weapon! Return to an NCC Agent immediately.");
+						EffectSound sound = SEffectManager.PlaySoundOnObject("ISFW_UnauthorizedAccess_SoundSet", player);
+						sound.SetSoundAutodestroy(true);
+						GetGame().GetCallQueue(CALL_CATEGORY_GAMEPLAY).CallLater(DisableSoundStart, 6500, false);
+					}
+					return;
+				}
+			}
+		}
+		
+		super.Fire(wpn);
+	}
+	
+	void DisableSoundStart()
+	{
+		soundStart = false;
 	}
 }
