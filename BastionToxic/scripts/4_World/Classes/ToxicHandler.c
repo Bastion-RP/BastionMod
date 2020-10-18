0246369d602658f8b1abbda4805acb17ac5a7072
KyHbKa <sashakulichev47@gmail.com>
2020-10-18 14:16:45 +1000

    toxic parallelepipede triggers

--- BastionToxic/scripts/4_World/Classes/ToxicHandler.c
+++ BastionToxic/scripts/4_World/Classes/ToxicHandler.c
@@ -3,12 +3,26 @@ class ToxicHandler
 	private PlayerBase player;
 	private ref array<ref ToxicAreaData> toxicAreas;
 	private ref array<string> protectableMasks;
+	private bool IsDebug;
+
+	// Debug stuff
+	// private Shape shapeMin;
+	// private Shape shapeMax;
+	private Shape playerShape;
+	private array<Shape> triggerShapes;
+	//============
 
 	void ToxicHandler()
 	{
 		player = PlayerBase.Cast(GetGame().GetPlayer());
 		toxicAreas = GetToxicManager().GetConfig().GetZones();
 		protectableMasks = GetToxicManager().GetConfig().GetMasks();
+
+		IsDebug = GetToxicManager().GetSettings().IsDebug();
+
+		if (IsDebug)
+			DebugStart();
+		//Debug.DestroyAllShapes();
 	}
 
 	void ~ToxicHandler()
@@ -55,6 +69,62 @@ class ToxicHandler
 
 	bool PlayerInToxicArea(ToxicAreaData toxicArea)
 	{
-		return (vector.Distance(player.GetPosition(), toxicArea.GetPos()) <= toxicArea.GetRadius());
+		if (toxicArea.IsCircle())
+			return (vector.Distance(player.GetPosition(), toxicArea.GetPos()) <= toxicArea.GetRadius());
+		else
+		{
+			if (CheckPos(toxicArea))
+			{
+				//Print("Player in box trigger");
+				return true;
+			}
+		}
+		return false;
+	}
+
+	bool CheckPos(ToxicAreaData toxicArea)
+	{
+		vector tmins; //trigger mins
+		vector tmaxs; //trigger maxs
+		vector pmins; //player mins
+		vector pmaxs; //player maxs
+		vector pos = toxicArea.GetPos();
+		tmins = pos + toxicArea.GetMins();
+		tmaxs = pos + toxicArea.GetMaxs();
+
+		// shape1.SetColor(Colors.BLACK);
+		// shape2.SetColor(Colors.BLACK);
+
+		GetPBounds(pmins, pmaxs, player.GetPosition() );
+		if (Math3D.CheckBoundBox(pmins, pmaxs, tmins, tmaxs))
+		{
+			// shape1.SetColor(Colors.RED);
+			// shape2.SetColor(Colors.RED);
+			return true;
+		}
+		return false;	
+	}
+
+	void GetPBounds(out vector min, out vector max, vector pos)
+	{
+		vector temp[2];
+		player.GetCollisionBox(temp);
+		min = temp[0] + pos;
+		max = temp[1] + pos;
+	}
+
+	void DebugStart()
+	{
+		// triggerShapes = new array<Shape>();
+
+		foreach (ToxicAreaData toxicArea : toxicAreas)
+		{
+			if (toxicArea.IsCircle())
+				Debug.DrawSphere(toxicArea.GetPos(), toxicArea.GetRadius(), Colors.RED);
+			else
+			{
+				Debug.DrawBox(toxicArea.GetPos() + toxicArea.GetMins(), toxicArea.GetPos() + toxicArea.GetMaxs(), Colors.RED);
+			}
+		}
 	}
 }
