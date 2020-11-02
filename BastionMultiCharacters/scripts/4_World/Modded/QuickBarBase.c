16675412640211ffa9a45142c53ef53a0f1cd5fc
Bryan Rode <51579862+OGRoomService@users.noreply.github.com>
2020-11-01 21:23:03 -0500

    Removed debugging for quickbar

--- BastionMultiCharacters/scripts/4_World/Modded/QuickBarBase.c
+++ BastionMultiCharacters/scripts/4_World/Modded/QuickBarBase.c
@@ -16,33 +16,4 @@ modded class QuickBarBase {
         }
         return arrQuickBarItems;
     }
-
-    override void _SetEntity( EntityAI entity, int index, bool force = false) {
-        Print("SETTING ENTITY ON QUICKBAR!!! " + entity + " | " + index);
-        super._SetEntity(entity, index, force);
-    }
-
-    override void OnSetEntityRequest(ParamsReadContext ctx)
-	{
-		EntityAI eai = null;
-		if (!ctx.Read(eai)) {
-            Print("FAILED TO READ " + eai);
-            return;
-        }
-		
-		int index = -1;
-		if (!ctx.Read(index)) {
-            Print("FAILED TO READ " + index);
-            return;
-        }
-		
-		bool force = false;
-		if (!ctx.Read(force)) {
-            Print("FAILED TO READ " + force);
-            return;
-        }
-        Print("SETTING ENTITY SHORTCUT!");
-		
-		_SetEntityShortcut(eai, index, force);
-    }
 }
